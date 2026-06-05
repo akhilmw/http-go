c55b0739f6222b9bef8421bc9f95f2aea2850104
akhilmw <akhilmw2@gmail.com>
2026-06-05 00:55:11 -0500

    refactor: extract file reading logic into getLinesChannel

--- main.go
+++ main.go
@@ -2,65 +2,80 @@ package main
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
-func main() {
+// this returns channel of string
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
 
-	f, err := os.Open("messages.txt")
-	if err != nil {
-		fmt.Println("Some error : ", err)
-	}
+	go func() {
+		defer f.Close()
+		defer close(ch)
 
-	defer f.Close()
+		parts := make([][]byte, 0)
 
-	parts := make([]any, 0, 8)
+		for {
 
-	for {
-		buffer := make([]byte, 8)
-		n, err := f.Read(buffer)
-		if n > 0 {
-			currentChunk := buffer[:n]
-			for {
-				lastLineIdx := bytes.IndexByte(currentChunk, '\n')
-				if lastLineIdx == -1 {
-					parts = append(parts, currentChunk)
-					break
-				} else {
+			buffer := make([]byte, 8)
+			n, err := f.Read(buffer)
+
+			if n > 0 {
+				currentChunk := buffer[:n]
+				for {
+					lastLineIdx := bytes.IndexByte(currentChunk, '\n')
+					if lastLineIdx == -1 {
+						parts = append(parts, currentChunk)
+						break
+					}
 					actual := currentChunk[:lastLineIdx]
 					parts = append(parts, actual)
 					// traverse the current state of parts that will hold the current line
-					var completeLine string
-					for _, p := range parts {
-						byteSlice := p.([]byte)
-						completeLine += string(byteSlice)
+					var completeLine strings.Builder
+					for _, part := range parts {
+						completeLine.Write(part)
 					}
 
-					fmt.Printf("read : %s\n", completeLine)
+					// fmt.Printf("read : %s\n", completeLine.String())
+					ch <- completeLine.String()
 					parts = parts[:0]
 
 					currentChunk = currentChunk[lastLineIdx+1:]
 					if len(currentChunk) == 0 {
 						break
 					}
+
 				}
 			}
-		}
 
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				// Print "read: end" as required by your test assertion
-				fmt.Println("read: end")
-				fmt.Println("File reading complete")
+			if err == io.EOF {
 				return
 			}
-			fmt.Println("Read error:", err)
-			return
+
+			if err != nil {
+				return
+			}
 		}
 
+	}()
+
+	return ch
+
+}
+
+func main() {
+
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Println("Some error:", err)
+		return
+	}
+
+	for line := range getLinesChannel(f) {
+		fmt.Printf("read: %s\n", line)
 	}
 
 }
